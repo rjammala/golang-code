b8a80132723c93f126593f757d6c1d7d5ea4870e
rjammala <rajanikanth@new-host-3.home>
2013-10-20 02:59:18 -0400

    Collatz problem brute force method

---
+++ collatz.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func findLength(n uint64) int {
+	length := 1
+	
+	for n > 1 {
+		if n % 2 == 0 {
+			n = n/2
+			length++
+		} else {
+			n = 3*n + 1
+			length++
+		}
+	}
+
+	return length
+}
+
+func main() {
+	
+	max := 0
+	for i := 2; i < 1000000; i++ {
+		tempResult := findLength(uint64(i))
+		if tempResult > max {
+			max = tempResult
+		}
+	}
+	
+	fmt.Println(max)
+}
